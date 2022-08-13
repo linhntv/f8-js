51e099f002a21767d85d27262c28e8859e94402b
Linh <nguyenvanlinhdt14svl02@gmail.com>
2022-08-13 09:06:53 +0700

    promise.resolve

--- main.js
+++ main.js
@@ -2,25 +2,9 @@
 
 
 
-function sleep(ms){
-    return new Promise(function(resolve){
-        setTimeout(resolve,ms)
+var promise = Promise.resolve([1,2,3]);
+promise
+    .then(function(result){
+        console.log(result)
     })
-} 
-sleep(1000)
-    .then(function(){
-        console.log(1)
-        return sleep(1000)
-    })
-    .then(function(){
-        console.log(2)
-        return sleep(1000)
-    })
-    .then(function(){
-        console.log(3)
-        return sleep(1000)
-    })
-    .then(function(){
-        console.log(4)
-        return sleep(1000)
-    })
+    .catch()
