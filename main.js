cb6ea407d3d42ef5b31e3c60e77e3ccfc4c04b2e
Linh <nguyenvanlinhdt14svl02@gmail.com>
2022-08-13 10:28:34 +0700

    promise.all

--- main.js
+++ main.js
@@ -2,11 +2,21 @@
 
 
 
-var promise = Promise.reject('có lỗi');
-promise
-    .then(function(result){
-        console.log(result)
-    })
-    .catch(function(err){
-        console.log('err:',err)
-    })
+var promise1= new Promise(function(resolve){
+   setTimeout(function(){
+    resolve([1]);
+   },1000)
+})
+let promise2= new Promise(function(resolve){
+    setTimeout(function(){
+        resolve([2,3]);
+    },2000)
+})
+Promise.all([promise1,promise2])
+    .then(
+        function(result){
+           let result1 = result[0]
+           let result2 = result[1]
+           console.log(result1.concat(result2))
+        }
+    )
