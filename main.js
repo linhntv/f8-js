46fa90fa47e863c9cc87792167fdd9fb0fe57718
Linh <nguyenvanlinhdt14svl02@gmail.com>
2022-08-13 10:30:23 +0700

    promise.all.Ães6

--- main.js
+++ main.js
@@ -14,9 +14,8 @@ let promise2= new Promise(function(resolve){
 })
 Promise.all([promise1,promise2])
     .then(
-        function(result){
-           let result1 = result[0]
-           let result2 = result[1]
+        function([result1,result2]){
+           
            console.log(result1.concat(result2))
         }
     )
