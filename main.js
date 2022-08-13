7faa210981933498fa86bbefd444d71cd915797b
Linh <nguyenvanlinhdt14svl02@gmail.com>
2022-08-13 09:02:39 +0700

    promise sleep

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 
 
 
-let promise= new Promise(function(resolve){
-    
-}) ;
-promise(1000) 
-    .then(function(){
-        return new Promise(function(resolve){
-            setTimeout(function(){
-                resolve([1,2,3]);
-            },3000);
-        })
-    })
-    .then(function(data){
-        console.log(data);
-        return 2
-    })
-    .then(function(data){
-        console.log(data);
-    })
-    .catch(function(){
 
+function sleep(ms){
+    return new Promise(function(resolve){
+        setTimeout(resolve,ms)
+    })
+} 
+sleep(1000)
+    .then(function(){
+        console.log(1)
+        return sleep(1000)
+    })
+    .then(function(){
+        console.log(2)
+        return sleep(1000)
+    })
+    .then(function(){
+        console.log(3)
+        return sleep(1000)
+    })
+    .then(function(){
+        console.log(4)
+        return sleep(1000)
     })
